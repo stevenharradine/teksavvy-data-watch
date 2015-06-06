dee5ca3591d5bf59c263a6bdf08fa66d7543cec6
steven harradine <steven.harradine@telus.com>
2015-06-05 22:06:45 -0400

    sum of each bucket for the current month

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-var API_URL = "https://api.teksavvy.com/web/Usage/UsageRecords",
-    urllib = require('urllib');
+var API_URL              = "https://api.teksavvy.com/web/Usage/UsageRecords?$skip=40",
+    urllib               = require('urllib'),
+    onPeakDownloadTotal  = 0,
+    onPeakUploadTotal    = 0,
+    offPeakDownloadTotal = 0,
+    offPeakUploadTotal   = 0;
 
-urllib.request(API_URL, {
-	headers: {
-		"TekSavvy-APIKey": "<< YOURKEY >>"
-	}
-}, function (err, data, res) {
-	if (err) {
-		throw err; // you need to handle error 
-	}
+function downloadData (url) {
+	urllib.request(url, {
+		headers: {
+			"TekSavvy-APIKey": "<< YOURKEY >>"
+		}
+	}, function (err, rawData, res) {
+		if (err) {
+			throw err; // you need to handle error 
+		}
 
-	var data = JSON.parse(data.toString()).value;
+		var data = JSON.parse(rawData.toString()).value,
+		    functionStop  = false,
+		    nextLink = JSON.parse(rawData.toString())["odata.nextLink"];
 
-	data.forEach (function (currentDataElement) {
-		console.log (currentDataElement.Date);
+		data.reverse().forEach (function (currentDataElement, index, array) {
+			var year          = currentDataElement.Date.split("T")[0].split("-")[0],
+			    month         = currentDataElement.Date.split("T")[0].split("-")[1],
+			    day           = currentDataElement.Date.split("T")[0].split("-")[2],
+			    previousMonth = index >= 1 ? array[ index - 1 ].Date.split("T")[0].split("-")[1] : null;
+
+			if (index > 1 && month != previousMonth) {
+				functionStop = true;
+			}
+
+			if (!functionStop) {
+				onPeakDownloadTotal  += currentDataElement.OnPeakDownload,
+				onPeakUploadTotal    += currentDataElement.OnPeakUpload,
+				offPeakDownloadTotal += currentDataElement.OffPeakDownload,
+				offPeakUploadTotal   += currentDataElement.OffPeakUpload;
+			}
+		});
+
+		// data is Buffer instance
+		console.log ("Data provided in GB and OnPeak/OffPeak\n  " +
+			         "D: " + onPeakDownloadTotal.toFixed(2) + "/" + offPeakDownloadTotal.toFixed(2) + "\n  " +
+			         "U: " + onPeakUploadTotal.toFixed(2)   + "/" + offPeakUploadTotal.toFixed(2));
 	});
+}
 
-	// data is Buffer instance
-	console.log ();
-});
+downloadData (API_URL);
