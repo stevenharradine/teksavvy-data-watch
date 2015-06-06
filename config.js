63e8e96afea0debac0b8ac94b18be04faf0f1b96
steven harradine <steven.harradine@telus.com>
2015-06-06 00:23:37 -0400

    added email

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-module.exports.API_KEY = "<< YOURKEY >>";
-module.exports.DATA_CAP = "400";
+module.exports.API_KEY        = "<< YOURKEY >>";
+module.exports.DATA_CAP       = "400";
+module.exports.EMAIL_PROVIDER = 'Gmail';
+module.exports.EMAIL_USER     = 'youremail';
+module.exports.EMAIL_PASSWORD = 'yourpassword';
